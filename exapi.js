0b5346ade9b9534ecd9678247b4549308e1db272
Jason Walzak <walzak@gmail.com>
2017-08-20 21:51:57 -0400

    Made weather.js made api template

---
+++ exapi.js
@@ -0,0 +1,35 @@
+// The GET method
+const request = require('request-promise')
+
+const options = {
+  method: 'GET'
+  uri: 'https://risingstack.com'
+}
+
+request(options)
+.then(function(response){
+  // Success
+  })
+.catch(function(err) {
+  // Fail
+  })
+
+// Post method 
+const options = {
+  METHOD = 'post',
+  uri: 'https://risingstack.com/login',
+  body: {
+    foo: 'bar'
+  },
+  json: true
+  // JSON will automatically stringify the body
+}
+
+request(options)
+  .then(function (response) {
+    // Handle response
+    }
+  .catch(function (err) {
+    // Handle error
+  })
+
